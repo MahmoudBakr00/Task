5a870dc41389e053aaaeac81d1ed7417e3bfcadc
MahmoudBakr00 <mahmoudbakr0780@gmail.com>
2026-08-11 12:03:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-
-Config · JS
 // ============================================================
 // حط بيانات مشروع Supabase بتاعك هنا
 // (Project Settings -> API -> Project URL / anon public key)
 // ============================================================
 const SUPABASE_URL = 'https://tzjegripbztrqdfvretb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_E2ZLXk6cHRevtZZY5LWlmA_-LSpNFYJ';
-  
